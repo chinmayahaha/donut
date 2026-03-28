7b4500e4e29819f1ad63f0fc5bb034a73b9ade0a
chinmayahaha <chinmayacc98@gmail.com>
2026-03-28 15:29:51 +0530

    fix pre-save hook, auth register working

--- server/models/User.js
+++ server/models/User.js
@@ -55,10 +55,9 @@ const UserSchema = new mongoose.Schema(
 
 
 // ── Hash password before saving ───────────────────────────────
-UserSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next(); // only hash if changed
+UserSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 // ── Instance method: compare password on login ────────────────
