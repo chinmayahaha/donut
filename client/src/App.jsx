d24c87e4a464613b484142ff2d4d5c7fb39db83c
chinmayahaha <chinmayacc98@gmail.com>
2026-03-29 23:05:14 +0530

    feat: protected routes with ProtectedRoute component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,17 @@ import LoginForm from './components/LoginForm'
 import Dashboard from './components/Dashboard'
 import SignupForm from './components/SignupForm'
 import ProjectsPage from './components/ProjectsPage'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<LoginForm />} />
         <Route path="/login" element={<LoginForm />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-<Route path="/signup" element={<SignupForm />} />
+        <Route path="/signup" element={<SignupForm />} />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/projects" element={<ProtectedRoute><ProjectsPage /></ProtectedRoute>} />
       </Routes>
     </BrowserRouter>
   )
